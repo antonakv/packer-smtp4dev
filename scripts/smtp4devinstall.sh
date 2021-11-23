ef0a2deb23160e226042adb05d19717019a5d2d1
Anton Akulov <antonakv@users.noreply.github.com>
2021-11-23 15:43:13 +0100

    Working tested configuration

--- scripts/smtp4devinstall.sh
+++ scripts/smtp4devinstall.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
-
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo systemctl enable docker
 cd /home/ubuntu
-docker-compose up -d
+sudo docker-compose up -d
